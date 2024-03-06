33163362b70a981aa29a701427adfb40949546ab
Wisney <wisneymaciel2@hotmail.com>
2024-03-06 18:27:48 -0300

    adicionado models e migrations

---
+++ database/migrations/2024_03_06_211749_create_pixels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePixelsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('pixels', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->foreign('store_id')->references('id')->on('store');
+            $table->text('platform');
+            $table->text('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('pixels');
+    }
+}
